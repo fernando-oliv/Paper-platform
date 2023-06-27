8f8e05d5fc9131b1da55d14564ff6e9e742ad83c
fernando-oliv <fernandooliveira@estudante.ufscar.br>
2023-06-27 13:39:31 -0300

    inicio em pygames

--- main.py
+++ main.py
@@ -1,72 +1,51 @@
 import pygame
 
-
 pygame.init()
 
-
-# Set the width and height of the output window, in pixels
-
-WIDTH = 800
-
+#some variables
+WIDTH = 800 # Set the width and height of the output window, in pixels
 HEIGHT = 600
-
+p_width = 40  # Set the player width and height
+p_height = 60
+p_x = 50    # Set the player positions 
+p_y = 440
+vel = 10
 
 # Set up the drawing window
-
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 
+def playerMovement(x, y, vel):
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:
+        y += vel
+
+    return (x,y)
 
 # Run until the user asks to quit
-
 running = True
 
 while running:
-
-
     # Did the user click the window close button?
-
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
-
             running = False
 
-
     # Fill the background with white
-
     screen.fill((255, 255, 255))
 
-
-    # Draw a blue circle with a radius of 50 in the center of the screen
-
-    pygame.draw.circle(screen, (0, 0, 255), (WIDTH // 2, HEIGHT // 2), 50)
-
-
-    # Draw a red-outlined square in the top-left corner of the screen
-
-    red_square = pygame.Rect((50, 50), (100, 100))
-
-    pygame.draw.rect(screen, (200, 0, 0), red_square, 1)
-
-
-    # Draw an orange caption along the bottom in 60-point font
-
-    text_font = pygame.font.SysFont("any_font", 60)
-
-    text_block = text_font.render(
-
-        "Hello, World! From Pygame", False, (200, 100, 0)
-
-    )
-
-    screen.blit(text_block, (50, HEIGHT - 50))
-
+    p_x, p_y = playerMovement(p_x, p_y, vel)
+    
+    #Draw the player
+    pygame.draw.rect(screen, (000, 100, 0), (p_x, p_y, p_width, p_height), 3)
 
     # Flip the display
-
     pygame.display.flip()
-
-
-# Done! Time to quit.
 
 pygame.quit()

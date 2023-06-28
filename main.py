86d12c4469095974e1f8d7e75ce02b3119227e16
fernando <fernandooliveira@estudante.ufscar.br>
2023-06-27 22:15:52 -0300

    refatorando a main para um código orientado a objetos

--- main.py
+++ main.py
@@ -1,51 +1,77 @@
 import pygame
+from pygame.locals import *
+ 
+class Player:
+    def __init__(self):
+        self.width = 40  # Set the player width and height
+        self.height = 60
+        self.x = 50    # Set the player positions 
+        self.y = 300
+        self.vel = 5
+        self.max_jump = 1
+        self.jump_count = self.max_jump
 
-pygame.init()
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.x -= self.vel
+        if keys[pygame.K_RIGHT]:
+            self.x += self.vel
+        if keys[pygame.K_UP]:
+            self.y -= self.vel
+        if keys[pygame.K_DOWN]:
+            self.y += self.vel
 
-#some variables
-WIDTH = 800 # Set the width and height of the output window, in pixels
-HEIGHT = 600
-p_width = 40  # Set the player width and height
-p_height = 60
-p_x = 50    # Set the player positions 
-p_y = 440
-vel = 10
 
-# Set up the drawing window
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
-
-def playerMovement(x, y, vel):
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_LEFT]:
-        x -= vel
-    if keys[pygame.K_RIGHT]:
-        x += vel
-    if keys[pygame.K_UP]:
-        y -= vel
-    if keys[pygame.K_DOWN]:
-        y += vel
-
-    return (x,y)
-
-# Run until the user asks to quit
-running = True
-
-while running:
-    # Did the user click the window close button?
-    for event in pygame.event.get():
+class App:
+    def __init__(self):
+        self._running = True
+        self.size = self.weight, self.height = 640, 400
+        pygame.init()
+        self.display = pygame.display.set_mode([self.weight, self.height])
+        self.player = Player()
+  
+    def on_event(self, event):
         if event.type == pygame.QUIT:
-            running = False
+            self._running = False
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
+    def on_load(self):
+        #load new level
+        pass
 
-    p_x, p_y = playerMovement(p_x, p_y, vel)
-    
-    #Draw the player
-    pygame.draw.rect(screen, (000, 100, 0), (p_x, p_y, p_width, p_height), 3)
+    def on_cleanup(self):
+        #clears previously loaded levels
+        pass
 
-    # Flip the display
-    pygame.display.flip()
+    def on_quit(self):
+        #close the aplication
+        pygame.quit()
 
-pygame.quit()
+    def draw_window(self):
+        self.display.fill((255,255,255))
+        pygame.draw.rect(self.display, (255, 0, 0), (self.player.x, self.player.y, self.player.width, self.player.height), 3)
+        pygame.display.update()
+
+    def on_execute(self):
+ 
+        if( self._running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+
+            self.player.move()
+            self.draw_window()            
+            return True
+        else:
+            self.on_cleanup()
+            return False
+ 
+
+if __name__ == "__main__" :
+    theApp = App()
+    FPS = 60
+    teste = True
+    clock = pygame.time.Clock()
+    while(teste):
+        clock.tick(FPS)
+        teste = theApp.on_execute()
+    theApp.on_quit()
